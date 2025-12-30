a5a37407cfd1ab8c980d756de2ee68449aaff4c8
MolBioFreak <teaguechristian@gmail.com>
2025-12-29 20:58:36 -0600

    fix: Resolve remaining console warnings
    - Wrapped App with HotkeysProvider for BlueprintJS hotkeys
    - Added localStorage cleanup for corrupted OVE/TG entries
    - Fixed undefined color CSS warning

--- platform/frontend/src/App.tsx
+++ platform/frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { HotkeysProvider } from '@blueprintjs/core';
 import { Layout } from './components/Layout';
 import { Dashboard } from './components/Dashboard';
 import { JobSubmission } from './components/JobSubmission';
@@ -8,20 +9,21 @@ import { MolBioToolkit } from './components/MolBioToolkit';
 
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/submit" element={<JobSubmission />} />
-        <Route path="/results" element={<ResultsViewer />} />
-        <Route path="/designs" element={<ResultsViewer />} />
-        <Route path="/designs/:jobId" element={<ResultsViewer />} />
-        <Route path="/jobs/:jobId" element={<JobDetailPage />} />
-        {/* Molecular Biology Toolkit - OVE-based sequence editor */}
-        <Route path="/designer" element={<MolBioToolkit />} />
-      </Routes>
-    </Layout>
+    <HotkeysProvider>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/submit" element={<JobSubmission />} />
+          <Route path="/results" element={<ResultsViewer />} />
+          <Route path="/designs" element={<ResultsViewer />} />
+          <Route path="/designs/:jobId" element={<ResultsViewer />} />
+          <Route path="/jobs/:jobId" element={<JobDetailPage />} />
+          {/* Molecular Biology Toolkit - OVE-based sequence editor */}
+          <Route path="/designer" element={<MolBioToolkit />} />
+        </Routes>
+      </Layout>
+    </HotkeysProvider>
   );
 }
 
 export default App
-
