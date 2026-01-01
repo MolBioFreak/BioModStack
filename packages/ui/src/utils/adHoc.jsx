49d4353f1227f6ee7725b0a44dcb55cbdca8c2fc
MolBioFreak <teaguechristian@gmail.com>
2025-12-31 19:50:37 -0600

    fix(rfantibody): Pin torchdata 0.8.0 for DGL datapipes compatibility
    DGL 2.1.0 requires torchdata.datapipes which was removed in newer
    versions. Pin to 0.8.0 which has the required API.

--- packages/ui/src/utils/adHoc.jsx
+++ packages/ui/src/utils/adHoc.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { compose } from "redux";
 
 //adHoc allows you to add dynamic HOCs to a component
-export default func => WrappedComponent => props => {
+// Extract 'key' prop to prevent "key prop is being spread into JSX" error in React 19
+export default func => WrappedComponent => ({ key: _key, ...props }) => {
   const calledFunc = func(props);
   const composeArgs = Array.isArray(calledFunc) ? calledFunc : [calledFunc];
   const ComposedAndWrapped = compose(...composeArgs)(WrappedComponent);
