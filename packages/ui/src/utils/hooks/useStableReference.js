edb81863183ef5bef048ae9366d39c2e7a59c265
MolBioFreak <teaguechristian@gmail.com>
2026-04-21 15:21:04 -0500

    chore: prune legacy editor and ui workspaces
    (cherry picked from commit 8f047f3f9c529a9b5384e5cac577c85def7168f1)

--- packages/ui/src/utils/hooks/useStableReference.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useRef } from "react";
-
-export const useStableReference = value => {
-  const ref = useRef();
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
-  return ref;
-};
